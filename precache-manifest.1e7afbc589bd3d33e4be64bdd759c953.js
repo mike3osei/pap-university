9563f6cf145afe07c9da90b3dddb09d742725ccd
Michael Osei <mike3osei@gmail.com>
2021-09-25 21:45:28 -0400

    Updates

---
+++ precache-manifest.1e7afbc589bd3d33e4be64bdd759c953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe52bfa546a4bb25064ab23a168b765",
+    "url": "/pap-university/index.html"
+  },
+  {
+    "revision": "dc4ad541d865c6605dc2",
+    "url": "/pap-university/static/css/main.d8a0f413.chunk.css"
+  },
+  {
+    "revision": "eda70b4aed196cdb32dc",
+    "url": "/pap-university/static/js/2.8c3c6f37.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pap-university/static/js/2.8c3c6f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a658fd06b12151744e",
+    "url": "/pap-university/static/js/3.51bc2f78.chunk.js"
+  },
+  {
+    "revision": "dc4ad541d865c6605dc2",
+    "url": "/pap-university/static/js/main.7c12deba.chunk.js"
+  },
+  {
+    "revision": "d0cccea1a444920f593d",
+    "url": "/pap-university/static/js/runtime-main.2444e590.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f751b51237c781e8f2fab6e939305248.js"
+  "/pap-university/precache-manifest.1e7afbc589bd3d33e4be64bdd759c953.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pap-university/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
