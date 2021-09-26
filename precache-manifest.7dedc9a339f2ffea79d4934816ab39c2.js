eb31e4b0598447da19c692aa1b576c3c96e145be
Michael Osei <mike3osei@gmail.com>
2021-09-25 21:07:51 -0400

    Updates

---
+++ precache-manifest.7dedc9a339f2ffea79d4934816ab39c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee08c6022d1fa2453d447645699d2021",
+    "url": "/pap-university/index.html"
+  },
+  {
+    "revision": "bf7877c2eefa9d9de39e",
+    "url": "/pap-university/static/css/main.d8a0f413.chunk.css"
+  },
+  {
+    "revision": "d4d9cdc406382292ae9c",
+    "url": "/pap-university/static/js/2.21a46fcc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/pap-university/static/js/2.21a46fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a658fd06b12151744e",
+    "url": "/pap-university/static/js/3.51bc2f78.chunk.js"
+  },
+  {
+    "revision": "bf7877c2eefa9d9de39e",
+    "url": "/pap-university/static/js/main.f7ff5836.chunk.js"
+  },
+  {
+    "revision": "d0cccea1a444920f593d",
+    "url": "/pap-university/static/js/runtime-main.2444e590.js"
+  }
+]);
